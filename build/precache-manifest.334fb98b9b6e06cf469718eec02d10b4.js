7b5c766af3dc00bec8684d43873e8594a06bde47
shivam <shivamj483@gmail.com>
2020-09-23 16:45:10 +0530

    Add files via upload

---
+++ build/precache-manifest.334fb98b9b6e06cf469718eec02d10b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e5327f863f0e840e654aea64410b48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d44bd6943de2f47a3fa",
+    "url": "/static/css/main.137a8630.chunk.css"
+  },
+  {
+    "revision": "e572900b0a325f98a801",
+    "url": "/static/js/2.a4a7719f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a4a7719f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d44bd6943de2f47a3fa",
+    "url": "/static/js/main.085f3fb0.chunk.js"
+  },
+  {
+    "revision": "de0d13683c0b751024c0",
+    "url": "/static/js/runtime-main.2b86c002.js"
+  },
+  {
+    "revision": "1a20727f8d794d3cb64077659e90f16a",
+    "url": "/static/media/Group 3.1a20727f.svg"
+  },
+  {
+    "revision": "b5361f453ca2c731c4f2addbc5c8af8b",
+    "url": "/static/media/hero.b5361f45.svg"
+  }
+]);
---
+++ src/components/Main.js
@@ -0,0 +1,85 @@
+import React, {useState} from "react";
+import hero from "../hero.svg";
+import { Link, useHistory } from "react-router-dom";
+
+const Main = () => {
+
+    const [joined, setJoined] = useState("");
+    const history = useHistory();
+
+
+    const join = (e) =>{
+        e.preventDefault();
+        if(joined !== ""){
+            let today = new Date();
+            let date = today.getFullYear()+ '-' +(today.getMonth()+1) + '-' + today.getDate();
+            history.push(`/thanks/${joined}`, date );
+        }
+
+    }
+
+
+
+    return(
+        <React.Fragment>
+            <header>
+                <img src={hero} />
+                <div>
+                    <h1>Mobile Application.</h1>
+                    <Link to="/contact">Get in touch</Link>
+                </div>
+          
+            </header>
+
+
+            <main>
+                <section className="services">
+                     <h2>Why this app is good For you?</h2>
+
+                     <div className="service-container">
+                         <div className="services-card service-one"></div>
+                         <div className="service-description">
+
+                            <h3>User Friendy.</h3>
+                         </div>
+                    </div>
+
+
+
+                    <div className="service-container">
+                      
+                      <div className="services-card service-two"></div>
+                      <div className="service-description">
+                          <h3>Clear &amp; Helpful.</h3>
+                      </div>
+                    </div>
+
+
+
+                    <div className="service-container">
+                        <div className="services-card service-three"></div>
+                        <div className="service-description">
+                            <h3>Support 24 hours.</h3>
+                        </div>
+                      
+                    </div>
+                </section>
+
+                <section>
+                    <h2>Send your email to get Early Access.</h2>
+
+                    <form className="newsletter" onSubmit={join}>
+                        <input type="email" placeholder="Your email goes here" onChange={(e) => setJoined(e.target.value)} />
+                        <input type="submit"  value="Join Now!"/>
+                    </form>
+
+                </section>
+            </main>
+
+
+        </React.Fragment>
+    )
+}
+
+
+export default Main;
